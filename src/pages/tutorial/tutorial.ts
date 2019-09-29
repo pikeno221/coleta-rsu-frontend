2d4d1df2cabc33eff9c85223829086693a2bcd7e
pikeno221 <gabriellgomesbh@gmail.com>
2019-09-29 14:40:03 -0300

    ajustando as telas iniciais de apresentacao

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -18,6 +18,17 @@ export class TutorialPage {
   slides: Slide[];
   showSkip = true;
   dir: string = 'ltr';
+  
+  title1: string = 'Bem-Vindo ao App Reciclart'
+  description1: string = 'Um app criado para incentivar e viabilizar a reutilização de Resíduos Sólidos Urbanos (RSUs)'
+  title2: string = 'Nossa Missão'
+  description2: string = 'O nosso objetivo é trazer para a palma da mão das pessoas dicas e conteudos relacionados as melhores práticas sobre o tema RECICLAGEM. '
+  title3: string = 'Alterações Climáticas'
+  description3: string = 'Faça parte da mudança e contribua para que as nossas futuras gerações não sofra as consequências das irresponsabilidades ambientais'
+  title4: string = 'Integração com Empresas de Recolhimento de Materiais Reutilizados'
+  description4: string = 'Interaja diretamente com parceiros e conheça a trajetória de nossos parceiros. Temos lindas histórias a compartilhar.'
+  title5: string = 'Pense, Use, Reuse'
+  description5: string = 'Agora que Você já está contextualizado, Vamos junto conosco, entre, descubra, aprenda, compartilhe e REUTILIZE!'
 
   constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService, public platform: Platform) {
     this.dir = platform.dir();
@@ -32,19 +43,24 @@ export class TutorialPage {
         console.log('Loaded values', values);
         this.slides = [
           {
-            title: values.TUTORIAL_SLIDE1_TITLE,
-            description: values.TUTORIAL_SLIDE1_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-1.png',
+            title: this.title1,
+            description: this.description1,
+            image: 'assets/img/logo-slidebox-1.jpg',
           },
           {
-            title: values.TUTORIAL_SLIDE2_TITLE,
-            description: values.TUTORIAL_SLIDE2_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-2.png',
+            title: this.title2,
+            description: this.description2,
+            image: 'assets/img/logo-slidebox-2.jpg',
           },
           {
-            title: values.TUTORIAL_SLIDE3_TITLE,
-            description: values.TUTORIAL_SLIDE3_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-3.png',
+            title: this.title3,
+            description: this.description3,
+            image: 'assets/img/logo-slidebox-3.jpg',
+          }, 
+          {
+            title: this.title4,
+            description: this.description4,
+            image: 'assets/img/logo-slidebox-4.png',
           }
         ];
       });
